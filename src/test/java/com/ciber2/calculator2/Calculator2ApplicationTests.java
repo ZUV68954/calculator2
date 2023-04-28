aba72217c21f78defdf37da91bbf91225a154165
administrador <zuv68954@educastur.es>
2023-04-28 20:38:33 +0200

    Añadido prueba unitaria.

--- src/test/java/com/ciber2/calculator2/Calculator2ApplicationTests.java
+++ src/test/java/com/ciber2/calculator2/Calculator2ApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ciber2.calculator2;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class Calculator2ApplicationTests {
+
+	private Calculator calculator = new Calculator();
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testSum() {
+		assertEquals(5, calculator.sum(2, 3));
+	}
 }
